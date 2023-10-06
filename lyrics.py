202929f9a79f62f0a419a3405c5e9a494adbe669
angelcepeda <jangelcepeda@protonmail.com>
2023-10-06 14:49:45 -0700

    artists

--- lyrics.py
+++ lyrics.py
@@ -1,27 +1,12 @@
 import threading
 import lyricsgenius as lg
-from lyricsgenius import OAuth2
 
-#GENIUS_TOKEN = "hbbR2NA8_o-1InmVM4KUcCfKtLRGcOyRqkL9waVpFHzJVs_6lWMcxuqLdntDAbaX"
-#genius = lg.Genius(GENIUS_TOKEN)
-client_id = "Lw1hYZTi0GwpPegSqsSW7b-zT1nf-xLSrnNAVp3cBR-_cgNC2A9hZy44qF6uhZup"
-redirect_uri = "threading-lyrics"
-auth = OAuth2.client_only_app(
-    client_id,
-    redirect_uri,
-    scope='all'
-)
-token = auth.prompt_user()
-genius = lg.Genius(token)
+GENIUS_TOKEN = "hbbR2NA8_o-1InmVM4KUcCfKtLRGcOyRqkL9waVpFHzJVs_6lWMcxuqLdntDAbaX"
+genius = lg.Genius(GENIUS_TOKEN)
 
 def go_for_my_lyrics(title):
-    lyrics = []
-    songs = genius.search_songs(title)
-    for song in songs['hits']:
-        url = song['result']['url']
-        song_lyrics = genius.lyrics(song_url = url)
-        lyrics.append(song_lyrics)
-    return lyrics
+    artist = genius.search_artist(title, max_songs=1, sort="title")
+    return artist.songs
 
 
 
