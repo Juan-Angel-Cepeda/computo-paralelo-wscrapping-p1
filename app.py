0f3f91bb3d114ba737cadd753ec0798a919316c8
angelcepeda <jangelcepeda@protonmail.com>
2023-10-06 09:54:55 -0700

    app, lyryic and requirements funcions

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import lyrics as ly
+
+st.title("Get your Lyrics")
+new_song = st.markdown('''## :red[Add your songs]''')
+song = st.text_input(label="Add your song",label_visibility="hidden")
+add_song = st.button("Add my song")
+
+if 'songs' not in st.session_state:
+    st.session_state.songs = []
+
+if add_song:
+    st.session_state.songs.append(song)
+
+st.markdown("## :green[Your songs]")
+for song in st.session_state.songs:
+    st.markdown(f"### {song}")
+    st.divider()
+
+if st.button("Get my lyirics"):
+    ly.call_lyrics(st.session_state.songs)
--- lyrics.py
+++ lyrics.py
@@ -1,5 +1,27 @@
 from lyrics_extractor import SongLyrics
-API_GOOGLE_KEY = ''
-ENGINE_ID = ''
+import threading
+import streamlit as st
+
+API_GOOGLE_KEY = st.secrets["API_GOOGLE_KEY"]
+ENGINE_ID = st.secrets["ENGINE_ID"]
+
+#for production
+#API_GOOGLE_KEY = st.secrets["API_GOOGLE_KEY"]
+#ENGINE_ID = st.secrets["ENGINE_ID"]
 
 extract_lyrics = SongLyrics(API_GOOGLE_KEY, ENGINE_ID)
+
+def go_for_my_lyrics(song):
+    lyrics = extract_lyrics.get_lyrics(song)
+    return lyrics
+
+def call_lyrics(songs):
+    threads = [threading.Thread(target=go_for_my_lyrics, args=(song,)) for song in songs]
+    for song in threads:
+        song.start()
+    
+    my_lyrics = []
+    for thread in threads:
+        my_lyrics.append(thread.join())
+    
+    return my_lyrics
