f511da25e2c192b52c0b84cefa8a02906493a909
angelcepeda <jangelcepeda@protonmail.com>
2023-10-06 13:54:45 -0700

    with genious

--- lyrics.py
+++ lyrics.py
@@ -1,18 +1,19 @@
-from lyrics_extractor import SongLyrics
 import threading
-import streamlit as st
+import lyricsgenius as lg
 
+GENIUS_TOKEN = "8dcLtoFObmQ1Vc1SJgbWtc7lfE2E_M9k_JFDdq-4YNmxhp-vl6LbB9WC0wZuOvEx"
+genius = lg.Genius(GENIUS_TOKEN)
 
-API_GOOGLE_KEY = st.secrets["API_GOOGLE_KEY"]
-ENGINE_ID = st.secrets["ENGINE_ID"]
+#API_GOOGLE_KEY = st.secrets["API_GOOGLE_KEY"]
+#ENGINE_ID = st.secrets["ENGINE_ID"]
 
-extract_lyrics = SongLyrics(API_GOOGLE_KEY, ENGINE_ID)
+#extract_lyrics = SongLyrics(API_GOOGLE_KEY, ENGINE_ID)
 
-def go_for_my_lyrics(song):
-    
-    lyrics = extract_lyrics.get_lyrics(song)
-        #lyrics = "Lyrics not found"
+def go_for_my_lyrics(title):
+    song = genius.search_song(title=title)
+    lyrics = song.lyrics
     return lyrics
+
 
 def call_lyrics(songs):
     threads = [threading.Thread(target=go_for_my_lyrics, args=(song,)) for song in songs]
