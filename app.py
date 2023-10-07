3fe3f41005ee87010e04d54263ae65cf5813e41a
angelcepeda <jangelcepeda@protonmail.com>
2023-10-07 11:06:29 -0700

    stats

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ st.title("Welocme to pokeScrapper")
 new_pokemon = st.markdown('''## :red[Add the pokemons you want to know about]''')
 pokemon = st.text_input(label="Add your pokemon",label_visibility="hidden")
 add_pokemon = st.button("Go to pokedex")
+pokeball = "./img/pokebola.png"
 
 if 'pokemons' not in st.session_state:
     st.session_state.pokemons = []
@@ -14,18 +15,20 @@ if add_pokemon:
 
 st.markdown("## :green[Your pokemons]")
 for pokemon in st.session_state.pokemons:
-    st.markdown(f"### {pokemon}")
+    st.image(pokeball, caption=pokemon, width=100)
     st.divider()
 
 if st.button("Get my pokemons"):
     pokeinfo = pkdx.run_pokedex(st.session_state.pokemons)
     num_pokemons = len(pokeinfo)
-    cols = st.columns(3)
+    cols = st.columns(len(st.session_state.pokemons))
     
     for i, (pokemon, info) in enumerate(pokeinfo.items()):
-        name, image, type = info
+        name, image, type, abilities = info
+        fig = pkdx.draw_stats(abilities)
         cols[i % 3].markdown(f"### {name}")
         cols[i % 3].image(image, caption=f"Image of {name}", use_column_width=True)
-        cols[i % 3].markdown(f"### Type: {type}")
+        cols[i % 3].markdown(f"### {type}")
+        cols[i % 3].markdown(f"### Abilities")
+        cols[i % 3].pyplot(fig)
         cols[i % 3].divider()
-
--- pokedex.py
+++ pokedex.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import threading
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def request(pokemon,pokedict,lock):
@@ -8,19 +10,43 @@ def request(pokemon,pokedict,lock):
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        print(soup)
         name = soup.find('title').text.split(' Pokédex')[0]
         image = soup.find('meta',property='og:image')['content']
         type = soup.find('a', class_='type-icon').text
-        #ability = soup.find('div', class_='column-7').text.strip()
-        pokeinfo = [name, image, type]
+        abilities = {}
+        vitals_tables = soup.find_all('table', class_='vitals-table',)
+        abilities_table = vitals_tables[3]
+        print(abilities_table)
+        for row in abilities_table.find_all('tr'):
+            th = row.find('th')
+            td = row.find('td',class_='cell-num')
+            if th and td:
+                abilities[th.text.strip()] = td.text.strip()
+            
+        pokeinfo = [name, image, type,abilities]
+        
         with lock:
             pokedict[pokemon] = pokeinfo    
+        
         return 
     else:
         print(f'Error al obtener el código {response.status_code}')
         return
+
+def draw_stats(abilities):
+    stats = list(abilities.keys())
+    values = [int(val) for val in abilities.values()]
+    angles = np.linspace(0, 2*np.pi, len(stats), endpoint=False).tolist()
+    values += values[:1]
+    angles += angles[:1]
     
+    fig, ax = plt.subplots()
+    ax.plot(angles, values, color='blue', alpha=0.25)
+    ax.set_yticklabels([])
+    ax.set_xticks(angles[:-1])
+    ax.set_xticklabels(stats)
+    return fig
+            
 def run_pokedex(pokemons):
     threads = []
     pokedict = {}
